ec9bf1724d024f241b07bb5777b60852d361a951
kajatrijsburg <kajatrijsburg@gmail.com>
2022-09-11 20:08:18 +0200

    adds the addEvent function to the queue object, also replaces all occurances of message with event

--- xEventQueue.lua
+++ xEventQueue.lua
@@ -21,24 +21,40 @@ function xEventQueue.newQueue(consumes, size)
         events = {},
         consumes = consumes,
         size = size,
-        start = 0,
-        index = 0
+        start = 1,
+        index = 1
     }
 
     function queue.addListner(self, listener)
+        --guard clauses
         utils.assertTable(listener)
         utils.assertTable(self)
         utils.assertTable(self.listeners)
         assert(self.listeners[listener.name] == nil, debug.traceback(listener.name .. " is already a listener to this event queue", 2))
         assert(listener.constant == true, debug.traceback("Listner needs to be constant. Did you forget to call finalize() on your listner?", 2))
         
+        --function body
         self.listeners[listener.name] = listener
     end
+    
+    --adds event to the queue
+    function queue.addEvent(self, event)
+        --guard clauses
+        utils.assertTable(self)
+        utils.assertTable(event)
+        assert(event.constant, debug.traceback("attempted to add an event to the queue that wasn't constant", 2))
 
-    function queue.addMessage(self, message)
+        --function body
+        table.insert(self.events, self.index, event)
+        self.index = self.index + 1
 
+        if self.index > size then
+            if self.consumes then 
+                error(debug.traceback("queue of size " .. self.size .. " ran out of space at index: " .. self.index), 2)
+            end
+            self.index = 1
+        end
     end
-
 
     return utils.protect(queue)
 end
@@ -46,23 +62,29 @@ end
 
 --creates a new listener
 function xEventQueue.newListener(name)
+    --guard clauses
     utils.assertString(name)
 
+    --function body
     local listener = {
         name = name
     }
 
-    function listener.addMessageType(self, messageType, onRecieve)
+    function listener.addEventType(self, eventType, onRecieve)
+        --guard clauses
         utils.assertTable(self)
-        utils.assertString(messageType)
+        utils.assertString(eventType)
         utils.assertFunction(onRecieve)
 
-        self[messageType] = onRecieve
+        --function body
+        self[eventType] = onRecieve
         return self
     end
 
     function listener.finalize(self)
+        --guard clauses
         utils.assertTable(self)
+
         self.addMessageType = nil
         return utils.constant(self)
     end
@@ -70,14 +92,14 @@ function xEventQueue.newListener(name)
     return listener
 end
 
-
-function xEventQueue.newMessage(reciever, data)
-    local message = {
+--creates a new event
+function xEventQueue.newEvent(reciever, data)
+    local event = {
         reciever = reciever,
         data = data
     }
 
-    return utils.constant(message)
+    return utils.constant(event)
 end
 
 return xEventQueue
