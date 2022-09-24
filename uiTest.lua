7b3ef833065ad0e1f12e85ce2ab220abd32c40dd
kajatrijsburg <kajatrijsburg@gmail.com>
2022-09-24 16:25:56 +0200

    added the capability to render text to the screen

--- uiTest.lua
+++ uiTest.lua
@@ -1,27 +1,79 @@
 local ui = require("xUIUtils")
 
-local screen = ui.newScreen("main")
+local screen = ui.newScreen("main", colors.black)
 
-local container = ui.newContainer("list", 1, 1, 10, 15, 1, "leftToRightTopToBottom", true)
+local container = ui.newContainer("list", 3, 5, 7, 10, 3)
+local container2 = ui.newContainer("list2", 1, 1, 10, 15, 2)
+
 
 container.appendItem(container,
     ui.newItem(
         "test1",
         function()
             print("Clicked!")
-        end, colors.red, colors.gray
+        end,
+        colors.red,
+        colors.gray,
+        {
+            newline = true,
+            padded = true
+        }
     )).appendItem(container,
     ui.newItem(
         "test2",
-        nil, colors.green, colors.gray
+        nil,
+        colors.green,
+        colors.gray,
+        {
+        }
     )).insertItem(container,
     ui.newItem(
-        "test4sfdsdfsdfsdfsdsdsdsdsdsdsdf",
+        "test3sfdsdfsdfsdfsdsdsdsdsdsdsdf",
         function()
             print("Clicked3!")
-        end, colors.green, colors.gray
+        end,
+        colors.green,
+        colors.black,
+        {
+            newline = true,
+        }
     ), 2)
 
-screen.addContainer(screen, container)
+container2.appendItem(container2,
+    ui.newItem(
+        "test4",
+        function()
+            print("Clicked!")
+        end,
+        colors.cyan,
+        colors.gray,
+        {
+
+        }
+    ))
+    .appendItem(container2,
+    ui.newItem(
+        "test5",
+        nil,
+        colors.black,
+        colors.white,
+        {
+
+        }
+    ))
+    .insertItem(container2,
+    ui.newItem(
+        "test6sfdsdfsdfsdfsdsdsdsdsdsdsdf",
+        function()
+            print("Clicked3!")
+        end,
+        colors.green,
+        colors.black,
+        {
+
+        }
+    ), 2)
+
+screen.addContainer(screen, container).addContainer(screen, container2)
 
 screen.draw(screen)
--- xUIUtils.lua
+++ xUIUtils.lua
@@ -7,9 +7,12 @@ local xUIUtils = {}
 -- a screen contains a number of containers which are lists of drawable items
 -- a screen can be drawn
 
-function xUIUtils.newScreen(name)
+function xUIUtils.newScreen(name, backgroundColor)
     utils.assertString(name)
+    utils.assertNumber(backgroundColor)
+
     local screen = {
+        backgroundColor = backgroundColor,
         name = name,
         containers = {
             count = 0
@@ -21,23 +24,34 @@ function xUIUtils.newScreen(name)
         utils.assertTable(container)
 
         self.containers[container.name] = container
+
+        table.sort(self.containers, function (k1, k2)
+            if type(k1) == "number" then
+                return true
+            end
+            if type(k2) == "number" then 
+                return false
+            end
+            return k1.depth < k2.depth
+        end)
+
         self.containers.count = self.containers.count + 1
         return self
     end
 
-    --loops over every container and draws it in order of 
+    --loops over every container and draws it
     function screen.draw(self)
-        print("drawing this screen")
-
-        --print(utils.tableToString(self.containers))
-        for index, value in ipairs(self.containers) do
-            print("test")
+        term.setBackgroundColor(self.backgroundColor)
+        term.clear()
+        for index, value in pairs(self.containers) do
             if index ~= "count" then
-                index.draw(index)
+                value.draw(value)
            end
         end
         return self
     end
+
+    
 
     return screen
 end
@@ -47,25 +61,14 @@ end
 -- to draw the container, draw the screen that it's on
 -- drawOrder dictates how the items in the container should be drawn
 -- valid draw orders are listed in the following table
-xUIUtils.drawStyles = {}
-xUIUtils.drawStyles["leftToRight"] = true
-xUIUtils.drawStyles["topToBottom"] = true
-xUIUtils.drawStyles["leftToRightTopToBottom"] = true
 
-function xUIUtils.newContainer(name, topLeftCornerX, topLeftCornerY, width, height, depth, drawOrder, wrap)
+function xUIUtils.newContainer(name, topLeftCornerX, topLeftCornerY, width, height, depth)
     utils.assertString(name)
     utils.assertNumber(topLeftCornerX)
     utils.assertNumber(topLeftCornerY)
     utils.assertNumber(width)
     utils.assertNumber(height)
     utils.assertNumber(depth)
-    utils.assertBoolean(wrap)
-
-    assert(xUIUtils.drawStyles[drawOrder],
-        debug.traceback("the draw order:" ..
-            drawOrder ..
-            " provided to container: " ..
-            name .. " is not a valid draw order. Valid draw orders are: " .. utils.tableToString(xUIUtils.drawStyles)), 2)
 
     local container = {
         name = name,
@@ -74,8 +77,6 @@ function xUIUtils.newContainer(name, topLeftCornerX, topLeftCornerY, width, heig
         width = width,
         height = height,
         depth = depth,
-        drawOrder = drawOrder,
-        wrap = wrap,
         items = {
             count = 0
         }
@@ -89,6 +90,8 @@ function xUIUtils.newContainer(name, topLeftCornerX, topLeftCornerY, width, heig
         self.items.count = self.items.count + 1
         return self
     end
+
+    
 
     function container.insertItem(self, item, position)
         utils.assertTable(item)
@@ -109,19 +112,85 @@ function xUIUtils.newContainer(name, topLeftCornerX, topLeftCornerY, width, heig
         return container
     end
 
+    --draw all the items in the container
     function  container.draw(self)
-        print("drawing container: " .. self.name)
+        --check if the container contains anythign to draw
+        --if there's nothing to render we don't have to draw
+        if self.items.count == 0 then
+            return self
+        end
+
+        --set the cursor to the topleft corner of the container to start drawing all the items
+        term.setCursorPos(self.topLeftCornerX, self.topLeftCornerY)
+
+        --loop over all the items in the container
+        for i = 1, self.items.count, 1 do
+            --get the item we're drawing
+            local item = self.items[i]
+            local textToDraw = item.text
+
+            --set the correct colors for this item
+            term.setBackgroundColor(item.backgroundColor)
+            term.setTextColor(item.foregroundColor)
+
+            --draw the items, moving to the next line if we ran out of space on this line of the container
+            while string.len(textToDraw) ~= 0 do
+                --figure out how many characters we can still draw on this line,. If we're out of space, move to the next line
+                local x, y = term.getCursorPos()
+                local spaceLeft = self.width - (x - self.topLeftCornerX)
+                if spaceLeft == 0 then
+                    term.setCursorPos(self.topLeftCornerX, y+1)
+                    spaceLeft = self.width
+                end
+                
+                --get the updated cursor position after that
+                x, y = term.getCursorPos()
+
+                --if we're out of vertical space in the container we can stop drawing this item
+                if y >= self.height + self.topLeftCornerY then
+                    break
+                end
+
+                --write as much text as we can fit in the remaining space
+                term.write(string.sub(textToDraw, 1, spaceLeft))
+                --remove the stuff we've drawn to the screen already from the stuff we still need to draw
+                textToDraw = string.sub(textToDraw, spaceLeft, string.len(textToDraw))
+            end
+
+            --if the item is supposed to be padded then fill the remaining space on the line with empty paces with the correct background color
+            if item.renderType.padded == true then
+                local x, y = term.getCursorPos()
+                local padding = ""
+                for i = 1, self.width - (x - self.topLeftCornerX), 1 do
+                    padding = padding .. " "
+                end
+                term.write(padding)
+            end
+
+            --if the item is supposed to have a new line after it, then move the cursor to a new line
+            if item.renderType.newline == true then
+                local x, y = term.getCursorPos()
+                term.setCursorPos(self.topLeftCornerX, y+1)
+            end
+        end
+
         return self
     end
     return container
 end
 
-function xUIUtils.newItem(text, onClick, foregroundColor, backgroundColor)
+function xUIUtils.newItem(text, onClick, foregroundColor, backgroundColor, renderType)
+    utils.assertString(text)
+    utils.assertNumber(foregroundColor)
+    utils.assertNumber(backgroundColor)
+    utils.assertTable(renderType)
+
     local item = {
         text = text,
         onClick = onClick,
         foregroundColor = foregroundColor,
-        backgroundColor = backgroundColor
+        backgroundColor = backgroundColor,
+        renderType = renderType
     }
 
     return item
